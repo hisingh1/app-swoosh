1acdeb1f57a373afbe1988c652cce275555f9863
Himmat Singh <himmat.sidhu521@gmail.com>
2020-11-14 13:05:17 -0800

    After Segues

---
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,23 @@
+//
+//  LeagueVC.swift
+//  app-swoosh
+//
+//  Created by Himmat Singh on 11/14/20.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+
+}
