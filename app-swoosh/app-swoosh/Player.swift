d720f7bfa0d635990abc4132102b6100108320ef
Himmat Singh <himmat.sidhu521@gmail.com>
2020-11-14 17:59:58 -0800

    Conditions on league page segue

---
+++ app-swoosh/app-swoosh/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Himmat Singh on 11/14/20.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String? 
+}
