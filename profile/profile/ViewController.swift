af034e31e7d5e39d7bea82b01f65b18acf0b275b
Himmat Singh <himmat.sidhu521@gmail.com>
2020-11-16 11:18:37 -0800

    Initial UI for iPhone

---
+++ profile/profile/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  profile
+//
+//  Created by Himmat Singh on 11/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var logoImg: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        logoImg.layer.cornerRadius = 10
+    }
+
+
+}
+
